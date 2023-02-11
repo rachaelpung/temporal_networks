1d64c32ed5328b3502cd4ac8c259af233d674cf0
rachaelpung <rachaelpung@hotmail.com>
2023-02-11 10:18:22 +0800

    create random and static network

--- code/temporal_functions.R
+++ code/temporal_functions.R
@@ -38,30 +38,64 @@ network_time <- function(net, net_meta, time_unit){
     # for high_school_2012 and high_school_2013
     net[, date_start:=as.Date(as.POSIXct(time_start, origin="1970-01-01", tz = 'Etc/GMT-7'))]
     net[, date_end:=as.Date(as.POSIXct(time_end, origin="1970-01-01", tz = 'Etc/GMT-7'))]
-    if(net[date_start!=date_end,.N] !=0) stop("contact overflowed to next day")
+    # if(net[date_start!=date_end,.N] !=0) stop("contact overflowed to next day")
     
     # for contacts that last between two intervals, separate them
     net_subset = net[step_start!=step_end]
     net = net[step_start==step_end]
     
-    net_subset = net_subset[rep(1:nrow(net_subset), each=2)]
-    net_subset[seq(1,nrow(net_subset),2), time_end:=NA]
-    net_subset[seq(2,nrow(net_subset),2), time_start:=NA]
+    # net_subset = net_subset[rep(1:nrow(net_subset), each=2)]
+    # net_subset[seq(1,nrow(net_subset),2), time_end:=NA]
+    # net_subset[seq(2,nrow(net_subset),2), time_start:=NA]
     
-    int = data.table(time=seq(net_meta$time_start, net_meta$time_end, time_unit))
+    
+    ##
+    rep_row=net_subset$step_end-net_subset$step_start+1
+    step = lapply(1:nrow(net_subset), function(x){
+      seq(net_subset[x,]$step_start,net_subset[x,]$step_end,1)
+    })
+    step=unlist(step)
+    net_subset = net_subset[rep(1:nrow(net_subset), times=rep_row)]
+    net_subset[, step_start:=step]
+    net_subset[, step_end:=step]
+    
+    test = copy(net_subset)
+    test[, time_start:=ifelse(row_number()==1,time_start, NA), by=.(node_i, node_j, time_start)]
+    
+    test[!cumsum(rep_row), time_end:=NA]
+    test[!(c(cumsum(rep_row)[1:length(rep_row)-1]+1,1)), time_start:=NA]
+    net_subset=copy(test)
+    
+    int = data.table(time=seq(net_meta$time_start, net_meta$time_end, time_unit*scale))
     int[,step:=1:nrow(int)]
-    net_subset[int, time:=i.time, on=c(step_end='step')]
+    
+    net_subset[, next_step:=step_start+1]
+    
+    net_subset[int, time:=i.time, on=c(next_step='step')]
     net_subset[is.na(time_end), time_end:=time]
+    
+    net_subset[int, time:=i.time, on=c(step_start='step')]
     net_subset[is.na(time_start), time_start:=time]
     
+    
+    ##
+    
+    # int = data.table(time=seq(net_meta$time_start, net_meta$time_end, time_unit*scale))
+    # int[,step:=1:nrow(int)]
+    # net_subset[int, time:=i.time, on=c(step_end='step')]
+    # net_subset[is.na(time_end), time_end:=time]
+    # net_subset[is.na(time_start), time_start:=time]
+    # 
     net_subset[, duration:=time_end-time_start]
     net_subset[, time:=NULL]
     
-    net_subset[, step_start:=time_start-net_meta$time_start]
-    net_subset[, step_start:=ceiling(step_start/(time_unit*scale))]
-    net_subset[, step_end:=time_end-net_meta$time_start]
-    net_subset[, step_end:=ceiling(step_end/(time_unit*scale))]
+    net_subset[, next_step:=NULL]
     
+    # net_subset[, step_start:=time_start-net_meta$time_start]
+    # net_subset[, step_start:=ceiling(step_start/(time_unit*scale))]
+    # net_subset[, step_end:=time_end-net_meta$time_start]
+    # net_subset[, step_end:=ceiling(step_end/(time_unit*scale))]
+    # 
     net = rbind(net,net_subset)
     setorder(net, node_i, node_j, time_start, time_end)
     rm(net_subset)
@@ -107,76 +141,55 @@ contact_time <- function(net){
 }
 
 
+# rewires network
+# takes an association matrix network, and returns a network in specified format ("graph","matrix","edgelist") 
+# under the specified null ("edge","deg","latt","clust").
 
+# retain all nodes
+# select time unit randomly
+# rewire the networks 
+# one iteration will be one static graph
+# multiple iterations will be one set of random graphs
 
-
-
-
-
-
-
-
-
-
-#' Generates aggregated and temporal weighted edgelist
-#' @param in_edgelist raw temporal edgelist
-# make_edgelist <- function(in_edgelist){
-#   
-#   out_edgelist = vector('list', 4)
-# 
-#   # raw
-#   out_edgelist[[4]] = copy(in_edgelist)
-#   
-#   record_days = max(c(in_edgelist$day_start, in_edgelist$day_end))
-#   record_duration = 133200 # (5+24+8) hr x 60 mins x 60 sec
-#   
-#   # scale = log(1-0.95)/-60*60 # 95% saturation on exponential function after 1 hour of contact
-#   # 
-#   # max_weight = 1
-#   # x0 = 45*60 # transmission risk is less than 5% for contacts lasting less than 30mins
-#   # k = log((max_weight/0.95)-1)/(-(60*60-x0)) # 95% saturation on logistic function after 1 hour of contact
-#   
-#   summary_edgelist = copy(in_edgelist)
-#   summary_edgelist = summary_edgelist[,.(cum_duration=sum(duration)), 
-#                                        by=.(node_i, node_j, day_start)]
-#   
-#   # temporal, linear/exponential/logistic function saturation
-#   # cumulative duration in a day
-#   summary_edgelist[day_start==1, weight:=cum_duration/(60*60*5)]
-#   summary_edgelist[day_start==2, weight:=cum_duration/(60*60*24)]
-#   summary_edgelist[day_start==3, weight:=cum_duration/(60*60*8)]
-#   out_edgelist[[1]] = copy(summary_edgelist)
-#   # out_edgelist[[2]] = copy(summary_edgelist[, weight:=(1-exp(-scale*cum_duration))])
-#   # out_edgelist[[3]] = copy(summary_edgelist[, weight:=max_weight/(1+exp(-k*(cum_duration-x0)))])
-#   
-#   
-#   summary_edgelist = summary_edgelist[,.(cum_duration=sum(cum_duration),
-#                                        contact_days=uniqueN(day_start)), 
-#                                       by=.(node_i, node_j)]
-#   
-#   # static, linear/exponential/logistic function saturation
-#   # cumulative duration
-#   out_edgelist[[2]] = copy(summary_edgelist[, weight:=cum_duration/(record_duration)])
-#   # out_edgelist[[5]] = copy(summary_edgelist[, weight:=1-exp(-scale*cum_duration)])
-#   # out_edgelist[[6]] = copy(summary_edgelist[, weight:=max_weight/(1+exp(-k*(cum_duration-x0)))])
-#   
-#   # mean duration of contact per day
-#   summary_edgelist = summary_edgelist[,.(mean_duration=(cum_duration/record_duration)*(60*60*24),
-#                                        prob_contact=contact_days/record_days),
-#                                       by=.(node_i, node_j)]
-#   
-#   # static, linear/exponential/logistic saturation 
-#   # mean duration, prob of contact
-#   out_edgelist[[3]] = copy(summary_edgelist[, weight:=prob_contact*mean_duration/(60*60*24)])
-#   # out_edgelist[[8]] = copy(summary_edgelist[, weight:=prob_contact*(1-exp(-scale*mean_duration))])
-#   # out_edgelist[[9]] = copy(summary_edgelist[, weight:=prob_contact*max_weight/(1+exp(-k*(mean_duration-x0)))])
-#  
-#   names(out_edgelist) = c('t_lin', 's_lin_cum', 's_lin_avg', 'raw')
-#   
-#   # names(out_edgelist) = c('t_lin', 't_exp', 't_log',
-#   #                           's_lin_cum', 's_exp_cum', 's_log_cum',
-#   #                           's_lin_avg', 's_exp_avg', 's_log_avg',
-#   #                           'raw')
-#    
-#   return(out_edgelist)
-# }
+network_null<-function(net,returns=c("graph","matrix","edgelist"),null=c("deg")){ 
+  
+  # sample network for a step as static network
+  # extract network for the next step 
+  # check how many nodes were retained
+  # randomise network in the next step and check how many were retained
+  # check the degree distribution in real life network in current step,
+  # real life network in next step and in the randomised network
+  
+  sample_step = sample(c(net$step),1)
+  n_node_net = uniqueN(c(net$node_i,net$node_j))
+  
+  net_i = net[step==sample_step,c('node_i', 'node_j')]
+  n_node_net_i = uniqueN(c(net_i$node_i, net_i$node_j))
+  
+  
+  net_i = graph_from_data_frame(net_i, directed=T) 
+  #convert to igraph object, check if need to amend for weights
+  
+  # check how many links were retains for net in next step 
+  
+  
+  n_edges = length(E(net_i))
+  n_nodes = length(V(net_i))
+  
+  if(null=="deg"){
+    # Degree null - using the same nodes and edges (and weights) and degree distribution (number of unique partners) 
+    # and re-shuffling the edges around them:
+    # Maintains: General network structure (n nodes, n links), daily consistency, 
+    # Individual differences in social contact propensity, daily consistency. 
+    # Randomises/losses: Clustering, hidden structure
+    net_r_i<-rewire(net_i,keeping_degseq(niter=n_edges))
+  }
+ 
+  
+  
+  #object return type:
+  # E(net_r_i)$weight<-eweights<-sample(E(net_i)$weight,length(E(net_i)$weight))
+  if(returns=="graph"){return(net_r_i)}
+  if(returns=="matrix"){return(as.matrix(as_adj(net_r_i,type="both",attr="weight")))}
+  if(returns=="edgelist"){return(cbind(as_edgelist(net_r_i),eweights))}
+}
--- code/temporal_step1_generate_networks.R
+++ code/temporal_step1_generate_networks.R
@@ -2,7 +2,7 @@ source("code/temporal_functions.R")
 source("code/temporal_library.R")
 
 # load edge list and meta data
-el_name = 'high_school_2012' # cruise_1
+el_name = 'cruise_1' # cruise_1, high_school_2012
 el = get(load(paste("data/el_", el_name, ".RData", sep=''))) 
 
 load("data/el_meta.RData")
@@ -65,12 +65,96 @@ plot(pmf_k0_k1[k0==k1]$k0, pmf_k0_k1[k0==k1]$P_cum, xlab='no of contact in curre
 lines(1:13, pmf_pois, col='red')
 
 
+# check if haslemere or cruise behave like french high school 
+# or work data if aggregate by time window
+# No, still retain original patterns
+
+# generate random and static networks with same degree distribution for comparison
+# sample network for a step as static network
+# extract network for the next step 
+# check how many nodes were retained
+# randomise network in the next step and check how many were retained
+# check the degree distribution in real life network in current step,
+# real life network in next step and in the randomised network
+
+net=copy(el)
 
 
+sample_step = sample(c(net$step),1)
+n_node = uniqueN(c(net$node_i,net$node_j))
+
+net_x = net[step==sample_step,c('node_i', 'node_j')]
+net_y = net[step==sample_step+1,c('node_i', 'node_j')]
+net_z = graph_from_data_frame(net_y, directed=T) 
+
+n_edges = length(E(net_z))
+n_nodes = length(V(net_z))
+net_z = rewire(net_z,keeping_degseq(niter=n_edges))
+net_z = as_edgelist(net_z)
+net_z = data.table(net_z)
+setnames(net_z, c('node_i', 'node_j'))
+
+pmf_k_x = net_x[,.N, by=.(node_i)]
+pmf_k_x = pmf_k_x[,.N, by=.(N)]
+setnames(pmf_k_x, c('k','N')); setorder(pmf_k_x)
+
+pmf_k_x = rbind(data.table(k=0, N=n_node-sum(pmf_k_x$N)), pmf_k_x)
+pmf_k_x[,P:=N/sum(N)]
 
 
+pmf_k_y = net_y[,.N, by=.(node_i)]
+pmf_k_y = pmf_k_y[,.N, by=.(N)]
+setnames(pmf_k_y, c('k','N')); setorder(pmf_k_y)
 
-# generate random and static networks for comparison
-# check if haslemere or cruise behave like french high school or work data
+pmf_k_y = rbind(data.table(k=0, N=n_node-sum(pmf_k_y$N)), pmf_k_y)
+pmf_k_y[,P:=N/sum(N)]
 
+pmf_k_z = net_z[,.N, by=.(node_i)]
+pmf_k_z = pmf_k_z[,.N, by=.(N)]
+setnames(pmf_k_z, c('k','N')); setorder(pmf_k_z)
+
+pmf_k_z = rbind(data.table(k=0, N=n_node-sum(pmf_k_z$N)), pmf_k_z)
+pmf_k_z[,P:=N/sum(N)]
+
+net_x[, step:=sample_step]; net_x[, contact:=1]
+net_y[, step:=sample_step+1]; net_y[, contact:=1]
+net_z[, step:=sample_step+1]; net_z[, contact:=1]
+
+net_x_temp = copy(net_x)
+net_x_rand = copy(net_x)
+
+net_x_temp[, next_step:=step+1]
+net_x_temp[net_y, next_contact:=i.contact, on=c(node_i='node_i', node_j='node_j', next_step='step')]
+net_x_temp[is.na(next_contact), next_contact:=0]
+
+net_x_rand[, next_step:=step+1]
+net_x_rand[net_z, next_contact:=i.contact, on=c(node_i='node_i', node_j='node_j', next_step='step')]
+net_x_rand[is.na(next_contact), next_contact:=0]
+
+net_x_temp=net_x_temp[,.(sum(contact), sum(next_contact)), by=.(node_i)]
+net_x_temp=net_x_temp[,.N, by=.(V1,V2)]
+setnames(net_x_temp, c('k0','k0_retain', 'N'))
+setorder(net_x_temp, k0, k0_retain)
+net_x_temp[,P:=N/sum(N), by=.(k0)]
+
+net_x_rand=net_x_rand[,.(sum(contact), sum(next_contact)), by=.(node_i)]
+net_x_rand=net_x_rand[,.N, by=.(V1,V2)]
+setnames(net_x_rand, c('k0','k0_retain', 'N'))
+setorder(net_x_rand, k0, k0_retain)
+net_x_rand[,P:=N/sum(N), by=.(k0)]
+
+
+for(i in 1:8){
+  pmf=copy(net_x_temp)
+  
+  if(i==1) plot(pmf[k0==i]$k0_retain, pmf[k0==i]$P, type='l', 
+                xlab='no. of contacts retained in next time unit', ylab='prop', col=colline[i],
+                xlim=c(0,8), ylim=c(0,1))  
+  if(i!=1) lines(pmf[k0==i]$k0_retain, pmf[k0==i]$P, col=colline[i])
+  if(i==8) legend("topright", legend=c("1 contact", "2 contact", '3 contact', '4 contact',
+                                       '5 contact','6 contact','7 contact', '8 contact'),
+                  col=colline, lty=1, cex=0.6)
+  
+  
+}
 
